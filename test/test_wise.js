b712e19e3335565dc124f4af6762eca4b868c257
cpmachado <cpmachado@protonmail.com>
2020-08-25 13:08:15 +0100

    Small code improvement, and adding wiseStreamedFinder

--- test/test_wise.js
+++ test/test_wise.js
@@ -6,7 +6,7 @@
  */
 
 const assert = require('assert').strict;
-const {wiseFinder} = require('../lib/wise');
+const {wiseFinder, wiseStreamedFinder} = require('../lib/wise');
 const {intSetGenerator} = require('../lib/util');
 
 
@@ -20,6 +20,19 @@ describe('wise', function() {
 
       it(`Should match ${deletedEntry}`, function() {
         assert.deepEqual(wiseFinder(testArray, deletedEntry), deletedEntry);
+      });
+    });
+  });
+  describe('#wiseStreamedFinder', function() {
+    const evens = Array(6).fill(0).map((_, i) => Math.pow(10, i + 1));
+    const sizes = evens.concat(evens.map((e) => e + 1));
+    sizes.forEach(function(size) {
+      const deletedEntry = Math.floor(Math.random() * size);
+      const testArray = intSetGenerator(size, deletedEntry);
+
+      it(`Should match ${deletedEntry}`, function() {
+        assert.deepEqual(wiseStreamedFinder(testArray, deletedEntry),
+            deletedEntry);
       });
     });
   });
